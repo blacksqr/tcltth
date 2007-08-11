201213229c9037be60eba52d2b978270bbeca66e
khomoutov <khomoutov@b933cb1f-2537-0410-922a-a9ff42f9b2a7>
2007-08-11 13:44:49 +0000

    First stage of rehashing of commands is done: * tclcmd.c is now tclinit.c -- it contains the package bootstrapping procedure   and exports the only external symbol Tcltth_Init. * Implementation of the "::tth::tth" command is moved to tcltth.c; * Registration of the "::tth::tth" command is moved to tcltth.c. * Added tcltth.h which defined an interface of tcltth.c to tclinit.c. * Made corresponding tweaks in configure.in.

---
+++ trunk/generic/tclinit.c
@@ -0,0 +1,68 @@
+/*
+ * tclinit.c --
+ *
+ *	This file initializes the "tcltth" package
+ *	arranging for any necessary commands to be created
+ *	and the package to be "provided".
+ *
+ * Copyright (c) 2007 Konstantin Khomoutov <flatworm@users.sourceforge.net>
+ *
+ * See the file "license.terms" for information on usage and redistribution
+ * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ *
+ * $Id$
+ *
+ */
+
+#include <tcl.h>
+
+#include "tcltth.h"
+
+#ifdef BUILD_tcltth
+#undef TCL_STORAGE_CLASS
+#define TCL_STORAGE_CLASS DLLEXPORT
+#endif /* BUILD_tcltth */
+
+EXTERN int	Tcltth_Init(Tcl_Interp * interp);
+
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * Tcltth_Init --
+ *
+ *	Initializes package "tcltth".
+ *
+ * Results:
+ *	A standard Tcl result
+ *
+ * Side effects:
+ *	The "tcltth" package is created.
+ *	One new command "::tth::tth" is added to the Tcl interpreter.
+ *
+ *----------------------------------------------------------------------
+ */
+
+int
+Tcltth_Init(Tcl_Interp *interp)
+{
+	/*
+	 * This may work with 8.0, but we are using strictly stubs here,
+	 * which requires 8.1.
+	 */
+	if (Tcl_InitStubs(interp, "8.1", 0) == NULL) {
+		return TCL_ERROR;
+	}
+	if (Tcl_PkgRequire(interp, "Tcl", "8.1", 0) == NULL) {
+		return TCL_ERROR;
+	}
+
+	if (TTH_CreateCmd(interp) == NULL) { return TCL_ERROR; }
+
+	if (Tcl_PkgProvide(interp, PACKAGE_NAME, PACKAGE_VERSION) != TCL_OK) {
+		return TCL_ERROR;
+	}
+
+	return TCL_OK;
+}
+

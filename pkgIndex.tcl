541f8b589177bdf5edbe8b010e9e725b76352da2
khomoutov <khomoutov@b933cb1f-2537-0410-922a-a9ff42f9b2a7>
2007-08-14 17:48:01 +0000

    * mmap() stuff made really switchable off in the sources. * ditto for test file. * fixed win/makefile.vc: brought in sync with the latest changes in the code. * added misc/hashdir.tcl which contains a simple proc to hash all files in a directory (non-recursively).

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -3,7 +3,7 @@
 #
 package ifneeded tth 0.1 \
 		[string map [list \$dir $dir] {
-    load [file join $dir libtth0.1.so] tth
+    load [file join $dir tth01.dll] tth
 	namespace eval ::tth { namespace export tiger tth }
 }]
 

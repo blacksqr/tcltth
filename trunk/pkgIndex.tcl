f721c0ff48bc5a48a8b55119db06b4f264a8296c
khomoutov <khomoutov@b933cb1f-2537-0410-922a-a9ff42f9b2a7>
2007-08-17 00:40:38 +0000

    * Removed Makefile and pkgIndex.tcl -- they must be generated by   ./configure on each platform during the build process. * Makefile and pkgIndex.tcl added to the ignore list of the root directory. * tests/intrinsics.test: added two tests for tth::tiger with -raw. * doc/tth.man: documentation updated.

--- trunk/pkgIndex.tcl
+++
@@ -1,9 +0,0 @@
-#
-# Tcl package index file
-#
-package ifneeded tth 0.1 \
-		[string map [list \$dir $dir] {
-    load [file join $dir tth01.dll] tth
-	namespace eval ::tth { namespace export tiger tth }
-}]
-
